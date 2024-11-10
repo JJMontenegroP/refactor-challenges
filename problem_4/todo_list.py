81e78aacde51b1559cb0cce55850f97493e71679
jjmontenegro <jj.montenegro@uniandes.edu.co>
2024-11-09 23:58:31 -0500

    Refactor problema_4

--- problem_4/todo_list.py
+++ problem_4/todo_list.py
@@ -1,18 +1,50 @@
 import os
 
-def todo_list():
-    file_path = "./refactoring/problema4/data/todo.txt"
+FILE_PATH = "./refactoring/problema4/data/todo.txt"
 
-    try:
-        file = open(file_path, 'r')
-        lines = file.readlines()
-        file.close()
-    except FileNotFoundError:
+def read_todo_list(file_path):
+    """Reads the to-do list from a file and returns a list of tasks."""
+    if not os.path.exists(file_path):
         print('No existe el archivo todo.txt')
-        lines = []
+        return []
     
-    lines = [line.rstrip() for line in lines]
+    with open(file_path, 'r') as file:
+        return [line.strip() for line in file.readlines()]
 
+def write_todo_list(file_path, tasks):
+    """Writes the current list of tasks to the file."""
+    with open(file_path, 'w') as file:
+        for task in tasks:
+            file.write(task + '\n')
+
+def display_tasks(tasks):
+    """Displays all tasks."""
+    print('-------------------------')
+    print('Tareas:')
+    for i, task in enumerate(tasks, start=1):
+        print(f'{i}. {task}')
+
+def add_task(tasks):
+    """Prompts the user to add a new task."""
+    task = input('Ingrese la tarea: ')
+    tasks.append(task)
+    print('Tarea agregada')
+
+def delete_task(tasks):
+    """Prompts the user to delete a task by its number."""
+    try:
+        task_number = int(input('Ingrese el número de la tarea a eliminar: '))
+        if 1 <= task_number <= len(tasks):
+            removed_task = tasks.pop(task_number - 1)
+            print(f'Tarea "{removed_task}" eliminada')
+        else:
+            print('Número de tarea inválido')
+    except ValueError:
+        print('Entrada inválida, por favor ingrese un número')
+
+def main_menu():
+    """Displays the main menu and handles user interactions."""
+    tasks = read_todo_list(FILE_PATH)
     print('Bienvenido a su lista de tareas. Estas son sus opciones:')
 
     while True:
@@ -23,33 +55,20 @@ def todo_list():
         print('4. Salir')
         print('-------------------------')
 
-        opcion = input('Ingrese una opcion: ')
+        option = input('Ingrese una opción: ')
 
-        if opcion == '1':
-            print('-------------------------')
-            print('Tareas:')
-            for i, line in enumerate(lines):
-                print('{}. {}'.format(i + 1, line))
-        elif opcion == '2':
-            tarea = input('Ingrese la tarea: ')
-            lines.append(tarea)
-            print('Tarea agregada')
-        elif opcion == '3':
-            tarea = input('Ingrese la tarea a eliminar: ')
-            if tarea.isdigit() and (0 < int(tarea) <= len(lines)):
-                lines.pop(int(tarea) - 1)
-                print('Tarea eliminada')
-            else: 
-                print('No existe la tarea')
-        elif opcion == '4':
+        if option == '1':
+            display_tasks(tasks)
+        elif option == '2':
+            add_task(tasks)
+        elif option == '3':
+            delete_task(tasks)
+        elif option == '4':
+            write_todo_list(FILE_PATH, tasks)
             print('Adios')
             break
-
-    file = open(file_path, 'w')
-    for line in lines:
-        file.write(line + '\n')
-    file.close()
-    
+        else:
+            print('Opción no válida. Intente de nuevo.')
 
 if __name__ == '__main__':
-    todo_list()
+    main_menu()
